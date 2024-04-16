ac8a3e742930f23671e70afe6ebf75dae39ce507
Fatih Gumus <fatihgumus72@gmail.com>
2024-04-16 23:12:10 +0300

    Initial Commit

---
+++ SwiftScrollView/SwiftScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScrollViewApp.swift
+//  SwiftScrollView
+//
+//  Created by Fatih Gümüş on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
